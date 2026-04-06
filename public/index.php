27e1f711e548156befa9a43c5f1f3c4e5c0183d6
OXWiz666 <htrbwiz@gmail.com>
2026-04-06 22:15:29 +0800

    deploy

--- public/index.php
+++ public/index.php
@@ -4,14 +4,27 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+/*
+|--------------------------------------------------------------------------
+| Determine the project base path
+|--------------------------------------------------------------------------
+| Supports two directory structures:
+|   Local dev:  public/ sits inside the project  -> base is ../
+|   Hostinger:  public_html/ is separate         -> base is ../sportscity/
+*/
+$basePath = match (true) {
+    file_exists(__DIR__.'/../sportscity/vendor/autoload.php') => __DIR__.'/../sportscity',
+    default => __DIR__.'/..',
+};
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $basePath.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once $basePath.'/bootstrap/app.php')
     ->handleRequest(Request::capture());
